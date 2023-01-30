e9b06ba9e7e1f279fee8ef610d0d22df16ae6939
Simon <simon.ahlstrom91@gmail.com>
2023-01-30 19:47:18 +0100

    Added pipline and test
    Added pipline and test for the pipeline

---
+++ file_io/lib/src/file_io.cpp
@@ -0,0 +1,68 @@
+#include "../include/FileIO.h"
+
+// Read a single line from a file and return it as a string
+std::string FileIO::readLine(const std::string &fileName)
+{
+    // create the var line with std::String.
+    std::string line;
+
+    // open the file with ifstream.
+    std::ifstream file(fileName);
+    if (file.is_open())
+    {
+        std::getline(file, line);
+        // Get the line from the file. store it in line.
+    }
+    else
+    {
+        std::cout << "Error opening file";
+    }
+
+    return line;
+}
+
+// Read all lines from a file and return them as a vector of strings
+std::vector<std::string> FileIO::readLines(const std::string &fileName)
+{
+    std::vector<std::string> lines;
+    // create the vector lines.
+    std::string line;
+    // create the var line.
+    std::ifstream file(fileName);
+    // open the file
+    if (file.is_open())
+    {
+        while (std::getline(file, line))
+        {
+            lines.push_back(line);
+        }
+    }
+    else
+    {
+        std::cout << "Error opening file";
+    }
+
+    // while getline is true then push line to the vector.
+    return lines;
+}
+
+// Write a single line to a file
+void FileIO::writeLine(const std::string &fileName, const std::string &line)
+{
+    std::ofstream file(fileName);
+    // open the file
+    file << line << std::endl;
+    // write to the file
+}
+
+// Write a vector of lines to a file
+void FileIO::writeLines(const std::string &fileName, const std::vector<std::string> &lines)
+{
+    std::ofstream file(fileName);
+    // open the file
+    for (const auto &line : lines)
+    {
+        file << line << std::endl;
+    }
+    // write to file for each line in lines.
+}
