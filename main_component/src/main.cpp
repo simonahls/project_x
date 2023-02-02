2ae1da818e78369e31a90a9f1118e86f3acd5ba3
Simon <simon.ahlstrom91@gmail.com>
2023-02-02 15:20:51 +0100

    Success build code v0.1
    First sucessed build for project_x

--- main_component/src/main.cpp
+++ main_component/src/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include "FileIO.h"
+#include <can_messages/CAN_min_signals.h>
 
-int main(void)
+void function_write_output();
+void function_read_input();
+// first function to check 3 first str.
+// secound check the secound word.
+// third check the value.
+
+// function pointers with a map.
+//  std:map< std::string ,
+int main(int argc, char *argv[])
 {
+
+    CAN_min_signals can;
+    // std::cout << FileIO::readLine("input_file.txt") << std::endl;
+    FileIO readline(void);
 
     // need to read from a file and execute the command with the help of fileio and can_messages.
 
