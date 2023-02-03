6fcafb191b91d9ac8545f5ff28a72d2c4e2293f5
Simon <simon.ahlstrom91@gmail.com>
2023-02-03 17:18:44 +0100

    Added experimental functions to main_component
    Played around with some write and read functions and fint function

--- main_component/src/main.cpp
+++ main_component/src/main.cpp
@@ -1,22 +1,44 @@
 #include <iostream>
 #include "FileIO.h"
+#include <string>
 #include <can_messages/CAN_min_signals.h>
 
-void function_write_output();
-void function_read_input();
-// first function to check 3 first str.
-// secound check the secound word.
-// third check the value.
+// SOME EXPERIMENTS FOR READING AND WRITING TO OUTPUT.......
+// Run command ./main_component ../../main_component/src/input_file.txt
 
-// function pointers with a map.
-//  std:map< std::string ,
 int main(int argc, char *argv[])
 {
-
+    std::string set{"set"};
+    std::string get{"get"};
+    std::string humidity{"humidity"};
+    std::string temperature{"temperature"};
+    std::cout << "korv" << std::endl;
+    FileIO Fileioobj;
     CAN_min_signals can;
-    // std::cout << FileIO::readLine("input_file.txt") << std::endl;
-    FileIO readline(void);
 
+    // can.set_humidity(10.5);
+    std::cout << can.set_humidity(10.5) << std::endl;
+
+    // This fucntion returns a line of string.
+    auto korv{Fileioobj.readLine("input_file.txt")};
+
+    std::cout << korv << std::endl;
+
+    if (korv.find("humidity") == 4)
+    {
+        std::cout << "call the humidity function!" << std::endl;
+    }
+
+    if (korv[0] == set[0])
+    {
+        std::cout << "CAll the set function" << std::endl;
+    }
+    if (korv[0] == get[0])
+    {
+        std::cout << "CAll the get function" << std::endl;
+    }
+
+    Fileioobj.writeLine("../../output.txt", Fileioobj.readLine(argv[1]));
     // need to read from a file and execute the command with the help of fileio and can_messages.
 
     // write to a file with the
