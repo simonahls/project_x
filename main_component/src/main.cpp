1bcec8a15959d9f16ef181d081b2dfcaa83e2752
Simon <simon.ahlstrom91@gmail.com>
2023-02-04 23:33:03 +0100

    Working read logic for main_component
    Read logic for main_component experimental workning.

--- main_component/src/main.cpp
+++ main_component/src/main.cpp
@@ -19,42 +19,97 @@ int main(int argc, char *argv[])
     // std::cout << can.set_humidity(10.5) << std::endl;
 
     // This fucntion returns a line of string.
-    auto korv{Fileioobj.readLine("input_file.txt")};
 
-    if (korv.substr(0, 3) == set)
+    auto whole_file{Fileioobj.readLines(argv[1])};
+    auto korv{Fileioobj.readLine(argv[1])};
+
+    // In this for loop i get access to one string at a time in the vector.
+    for (auto i : whole_file)
     {
-
-        //  std::cout << "Set dectected init set function....\nDone!\n"
-        //          << std::endl;
-        // need to know what function to call next set humidety or temperature
-        if (korv.substr(4, 3) == humidity)
+        // std::cout << "start" << std::endl;
+        std::cout << i << std::endl;
+        // std::cout << "end of loop" << std::endl;
+        if (i.substr(0, 3) == set)
         {
-            std::cout << "Set dectected, humidety dectected init humidety set_function....\nDone!" << std::endl;
-            auto value_hum = korv.substr(13, 10);
-            std::cout << value_hum << "<---printing value!" << std::endl;
+
+            //  std::cout << "Set dectected init set function....\nDone!\n"
+            //          << std::endl;
+            // need to know what function to call next set humidety or temperature
+            if (i.substr(4, 3) == humidity)
+            {
+                std::cout << "Set dectected, humidety dectected init humidety set_function....\nDone!" << std::endl;
+                auto value_hum = i.substr(13, (i.length() - 14));
+                std::cout << value_hum << "<---printing value!" << std::endl;
+            }
+            if (i.substr(4, 3) == temperature)
+            {
+
+                std::cout << "Set dectected, temperature dectected init temperature set_function....\nDone!" << std::endl;
+                auto value_temp = i.substr(16, (i.length() - 17));
+                std::cout << value_temp << "<---printing value!" << std::endl;
+            }
         }
-        if (korv.substr(4, 3) == temperature)
+        else if (i.substr(0, 3) == get)
         {
 
-            std::cout << "Set dectected, temperature dectected init temperature set_function....\nDone!" << std::endl;
-            auto value_temp = korv.substr(16, 10);
-            std::cout << value_temp << "<---printing value!" << std::endl;
+            // std::cout << "get dectected init get function....\nDone!\n"
+            //           << std::endl;
+            if (i.substr(4, 3) == humidity)
+            {
+                std::cout << "get dectected,humidety dectected init get_humidety function....\nDone!" << std::endl;
+                auto value_hum = i.substr(13, (i.length() - 14));
+                std::cout << value_hum << "<---printing value!" << std::endl;
+            }
+            if (i.substr(4, 3) == temperature)
+            {
+                std::cout << "get dectected,temperature dectected init get_temperature function....\nDone!" << std::endl;
+                auto value_temp = i.substr(16, (i.length() - 17));
+                std::cout << value_temp << "<---printing value!" << std::endl;
+            }
         }
     }
-    else if (korv.substr(0, 3) == get)
-    {
 
-        // std::cout << "get dectected init get function....\nDone!\n"
-        //           << std::endl;
-        if (korv.substr(4, 3) == humidity)
-        {
-            std::cout << "get dectected,humidety dectected init get_humidety function....\nDone!" << std::endl;
-        }
-        if (korv.substr(4, 3) == temperature)
-        {
-            std::cout << "get dectected,temperature dectected init get_temperature function....\nDone!" << std::endl;
-        }
-    }
+    // extract one line at a time then next one with for loop.
+    // can i return how many lines a file contains(do i need to know)
+
+    // if (korv.substr(0, 3) == set)
+    // {
+
+    //     //  std::cout << "Set dectected init set function....\nDone!\n"
+    //     //          << std::endl;
+    //     // need to know what function to call next set humidety or temperature
+    //     if (korv.substr(4, 3) == humidity)
+    //     {
+    //         std::cout << "Set dectected, humidety dectected init humidety set_function....\nDone!" << std::endl;
+    //         auto value_hum = korv.substr(13, 10);
+    //         std::cout << value_hum << "<---printing value!" << std::endl;
+    //     }
+    //     if (korv.substr(4, 3) == temperature)
+    //     {
+
+    //         std::cout << "Set dectected, temperature dectected init temperature set_function....\nDone!" << std::endl;
+    //         auto value_temp = korv.substr(16, 10);
+    //         std::cout << value_temp << "<---printing value!" << std::endl;
+    //     }
+    // }
+    // else if (korv.substr(0, 3) == get)
+    // {
+
+    //     // std::cout << "get dectected init get function....\nDone!\n"
+    //     //           << std::endl;
+    //     if (korv.substr(4, 3) == humidity)
+    //     {
+    //         std::cout << "get dectected,humidety dectected init get_humidety function....\nDone!" << std::endl;
+    //         auto value_hum = korv.substr(13, 10);
+    //         std::cout << value_hum << "<---printing value!" << std::endl;
+    //     }
+    //     if (korv.substr(4, 3) == temperature)
+    //     {
+    //         std::cout << "get dectected,temperature dectected init get_temperature function....\nDone!" << std::endl;
+    //         auto value_temp = korv.substr(16, 10);
+    //         std::cout << value_temp << "<---printing value!" << std::endl;
+    //     }
+    // }
 
     // std::cout << korv.substr(0, 3) << std::endl;
 
